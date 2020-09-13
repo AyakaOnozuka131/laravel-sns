13a8144458ba606b7e79b1f0e20fc332e4a85e47
AyakaOnozuka131 <c_yu_again@icloud.com>
2020-09-13 15:47:46 +0900

    :+1:Update ユーザー画面の作成

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,8 @@
 
 @section('title','ユーザー登録')
 
+@include('nav')
+@section('content')
 <div class="container">
   <div class="row">
     <div class="mx-auto col col-12 col-sm-11 col-md-9 col-lg-7 col-xl-6">
@@ -13,9 +15,11 @@
           <div class="card-text">
             <!-- routeは、与えられた名前付きルートのURLを返す -->
             <form action="{{ route('register') }}" method="post">
+              <!-- csrfは、Cross-Site Request Forgeries(クロスサイト・リクエスト・フォージェリ)というWebアプリケーションの脆弱性の略称で、上記のinputタグはこの脆弱性からWebサービスを守るためのトークン情報 -->
               @csrf
               <div class="md-form">
                 <label for="name">ユーザー名</label>
+                <!-- old関数:直前に入力した値を保持する関数・passwordとpassword_confirmationはnullを返すので使用しない -->
                 <input class="form-control" type="text" id="name" name="name" required value="{{ old('name') }}">
                 <small>英数字3〜16文字(登録後の変更はできません)</small>
               </div>
@@ -27,6 +31,7 @@
                 <label for="password_confirmation">パスワード(確認)</label>
                 <input class="form-control" type="password" id="password_confirmation" name="password_confirmation" required>
               </div>
+              <button class="btn btn-block blue-gradient mt-2 mb-2" type="submit">ユーザー登録</button>
             </form>
 
             <div class="mt-0">
