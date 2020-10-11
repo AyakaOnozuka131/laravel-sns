37fc4243b5910ede00a879160062b9da4bea68f9
AyakaOnozuka131 <c_yu_again@icloud.com>
2020-10-11 14:54:15 +0900

    :+1:Update Googleアカウントを使ったユーザー登録・ログイン機能

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,17 @@
 |
 */
 
-use App\Http\Controllers\ArticleController;
-
 Auth::routes();
 // Route::getの第一引数には、URLを文字列で渡す
 // 第二引数には、どのコントローラーで何のメソッドを実行するのかを文字列で渡す
 // コントローラー名とメソッド名の間には@を入れる。
 Route::prefix('login')->name('login.')->group(function () {
   Route::get('/{provider}', 'Auth\LoginController@redirectToProvider')->name('{provider}');
+  Route::get('/{provider}/callback', 'Auth\LoginController@handleProviderCallback')->name('{provider}.callback');
+});
+Route::prefix('register')->name('register.')->group(function () {
+  Route::get('/{provider}', 'Auth\RegisterController@showProviderUserRegistrationForm')->name('{provider}');
+  Route::post('/{provider}', 'Auth\RegisterController@registerProviderUser')->name('{provider}');
 });
 Route::get('/', 'ArticleController@index')->name('articles.index');
 Route::resource('/articles', 'ArticleController')->except(['index', 'show'])->middleware('auth');
@@ -31,13 +34,10 @@ Route::get('/tags/{name}', 'TagController@show')->name('tags.show');
 Route::prefix('users')->name('users.')->group(function () {
   Route::get('/{name}', 'UserController@show')->name('show');
   Route::get('/{name}/likes', 'UserController@likes')->name('likes');
-
   Route::get('/{name}/followings', 'UserController@followings')->name('followings');
   Route::get('/{name}/followers', 'UserController@followers')->name('followers');
-
   Route::middleware('auth')->group(function () {
-    Route::put('/{name}/follow', 'UserController@follow')->name('follow');
-    Route::delete('/{name}/follow', 'UserController@unfollow')->name('unfollow');
+      Route::put('/{name}/follow', 'UserController@follow')->name('follow');
+      Route::delete('/{name}/follow', 'UserController@unfollow')->name('unfollow');
   });
 });
-
